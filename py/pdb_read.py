df6414445be17845486a525d1707ac8dae7cf5c3
Louis <naivelocus@gmail.com>
2015-10-25 02:17:13 +0000

    Working PDB to VRML2 converter and 15 converted structures

--- py/pdb_read.py
+++ py/pdb_read.py
@@ -1,10 +1,21 @@
 # !/usr/bin/env python
 
 import chimera
+from chimera import runCommand as rc
 import sys
+import os
 
 for arg in sys.argv:
-    molecule = chimera.openModels.open(arg, type='PDB')
+    if (arg.startswith('basedir:')):
+        basedir = arg.split('basedir:')[1]
+        print basedir
+        # This is the base directory, used for saving files
+    if (len(arg) == 4):
+        pdb_code = arg.lower()
+        molecule = chimera.openModels.open(arg, type='PDB')
+        rc('wait 1')
+        rc('export '+basedir+'/structures/wrl'+'/'+pdb_code+'.wrl')
 
+# converted models to .fbx
 
-print "done reading models"
+rc('stop now')
